ad63dfeb7636993643f3a4314560ecd433f674bc
Dipesh Patel <dipeshpatel827@gmail.com>
2023-11-23 23:20:08 +0530

    Add project files.

---
+++ DIgiBharat/Program.cs
@@ -0,0 +1,49 @@
+using DIgiBharat.Model;
+using DIgiBharat.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+        };
+    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<GroupService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DIgiBharat/Service/GroupService.cs
@@ -0,0 +1,18 @@
+﻿using DIgiBharat.Model;
+
+namespace DIgiBharat.Service
+{
+    public class GroupService
+    {
+        private readonly AppDbContext _appDbContext;
+        public GroupService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public List<GroupModel> getall()
+        {
+
+            return _appDbContext.Group.ToList();
+        }
+    }
+}
