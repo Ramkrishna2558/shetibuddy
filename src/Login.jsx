78f9363aaa923ea45df9d1404b2f25815a753619
ram <ramkrishnamore299@gmail.com>
2023-11-23 22:26:29 +0530

    v

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,77 @@
-import React from 'react'
-import '../src/Login.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://your-dotnet-api-endpoint.com/login', formData);
+
+      if (response.status === 200) {
+        console.log('Login successful');
+      } else {
+        console.error('Login failed');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
- <>
+    <>
+      <div className="login-container justify-center flex w-2/4 bg-slate-500 flex-col p-8 ">
+        <form className="login-form flex justify-center flex-col " onSubmit={handleSubmit}>
+          <h2 className="text-white text-2xl mb-4">LOGIN</h2>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            className="username mb-2 p-2"
+            required
+            onChange={handleChange}
+          />
 
-    <div className="login-container">
-        
-        <form className="login-form">
-            <h2>LOGIN</h2>
-            <label for="username">Username:</label>
-            <input type="text" className="username" required/>
-            
-            <label for="password">Password:</label>
-            <input type="password" className="password" required/>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            className="password mb-2 p-2"
+            required
+            onChange={handleChange}
+          />
 
-            <button type="submit">Login</button>
-            <button className="registerButton" onclick="">Register User</button>
-            <a href="#" onclick="">Forgot Password?</a>
-
+          <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">
+            Login
+          </button>
+          <Link
+            to="/register"
+            className="registerButton bg-gray-500 text-white p-2 rounded hover:bg-gray-700"
+          >
+            Register User
+          </Link>
+          <a href="#" className="text-white" onClick={() => alert('Forgot Password?')}>
+            Forgot Password?
+          </a>
         </form>
+      </div>
 
-    </div>
-    
-    <footer>
+      <footer className="text-white text-center mt-4">
         <p>2023 Made By Ramkrishna More</p>
-    </footer>
- 
- </>
+      </footer>
+    </>
   );
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,34 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    newUsername: '',
+    newPassword: '',
+    securityQuestion: '',
+    securityAnswer: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://your-dotnet-api-endpoint.com/register', formData);
+
+      if (response.status === 200) {
+        console.log('User registered successfully');
+      } else {
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
-    <div className="registration-container" style={{ display: none }}>
-      <h2>REGISTER</h2>
-      <form className="registration-form">
+    <div className="registration-container justify-center flex w-2/4 bg-slate-700 flex-col p-8">
+      <h2 className="text-white text-2xl mb-4">REGISTER</h2>
+      <form className="registration-form flex justify-center flex-col" onSubmit={handleSubmit}>    
         <label htmlFor="new-username">New Username:</label>
         <input
           type="text"
           id="new-username"
-          className="new-username"
+          className="new-username mb-2 p-2"
           required
-   
-         
         />
 
         <label htmlFor="new-password">New Password:</label>
         <input
           type="password"
           id="new-password"
-          className="new-password"
+          className="new-password mb-2 p-2"
           required
-       
         />
 
         <label htmlFor="security-question">Security Question:</label>
         <select
           id="security-question"
-          className="security-question"
-   
+          className="security-question mb-2 p-2"
         >
           <option value="">Select a security question</option>
           <option value="last-name">What is your last name?</option>
@@ -41,19 +68,17 @@ export default function Register() {
         <input
           type="text"
           id="registration-security-answer"
-          className="registration-security-answer"
+          className="registration-security-answer mb-2 p-2"
           required
-       
         />
 
-        <button type="submit" >
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">
           Register user
         </button>
-        <button className="regLoginButton" >
+        <button className="regLoginButton bg-gray-500 text-white p-2 rounded hover:bg-gray-700">
           Login Now
         </button>
       </form>
     </div>
-    
-  )
+  );
 }
