a18c504d49e8e8b74945921faaf45f115b832cd6
Patel Dipesh <69524619+dipeshpatel02@users.noreply.github.com>
2024-02-04 11:17:46 +0530

    all minor files

--- DIgiBharat/obj/Debug/net6.0/DIgiBharat.AssemblyInfo.cs
+++ DIgiBharat/obj/Debug/net6.0/DIgiBharat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIgiBharat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813a68b0c45763568032a51a040b3b1ea3c9e28d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16658bdc4caa098e3d1d0abcdfb30f9303cc6a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIgiBharat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIgiBharat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
